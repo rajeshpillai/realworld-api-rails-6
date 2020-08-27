1ca8530e52c4a4a539d7d375ab1c5df7becc643c
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-08-27 18:09:26 +0530

    CORS

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,14 @@ module Demo
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.middleware.insert_before 0, Rack::Cors  do
+      allow do
+        origins '*'
+        resource '/api/*',
+                 headers: :any,
+                 methods: %i[get post delete put patch options head]
+      end
+    end
   end
 end
